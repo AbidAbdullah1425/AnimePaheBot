5f037e3a6fd1810ab1a9ee974ca0f0e8b674d8d2
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-07-16 12:15:15 +0600

    Update headers.py

--- plugins/headers.py
+++ plugins/headers.py
@@ -22,3 +22,21 @@ session.headers.update({
     'referer': 'https://animepahe.ru',
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
 })
+
+
+
+pahewin_headers = {
+    'authority': 'pahe.win',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'accept-language': 'en-US,en;q=0.9',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'referer': 'https://pahe.win/',
+    'sec-ch-ua': '"Not A(Brand";v="99", "Microsoft Edge";v="121", "Chromium";v="121"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1'
+}
