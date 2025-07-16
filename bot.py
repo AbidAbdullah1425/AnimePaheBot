f8b7ab484ab52e9759c33a1de32736dc60a353f9
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-07-16 08:36:19 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,23 +2,75 @@
 #..........Anyone Can Modify This As He Likes..........#
 #..........Just one requests do not remove my credit..........#
 
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from config import API_ID, API_HASH, BOT_TOKEN, ADMIN, LOG_CHANNEL
+
+from aiohttp import web
+from plugins import web_server
+from pyrogram import Client
+from pyrogram.enums import ParseMode
+import sys
+from datetime import datetime
+import pyrogram.utils
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+
+from config import API_HASH, API_ID, BOT_TOKEN, LOG_CHANNEL, ADMIN, TG_BOT_WORKERS, LOGGER
 from pyrogram import utils as pyroutils
 pyroutils.MIN_CHANNEL_ID = LOG_CHANNEL
-from config import API_ID, API_HASH, BOT_TOKEN, ADMIN, LOG_CHANNEL
 
-# Bot configuration
-API_ID = API_ID
-API_HASH = API_HASH
-BOT_TOKEN = BOT_TOKEN
-plugins = dict(root="plugins")
-# Initialize the bot
-app = Client("anime_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=plugins)
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="Bot",
+            api_hash=API_HASH,
+            api_id=API_ID,
+            plugins={
+                "root": "plugins"
+            },
+            workers=TG_BOT_WORKERS,
+            bot_token=BOT_TOKEN
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.uptime = datetime.now()
+
+        self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/codeflix_bots")
+        self.LOGGER(__name__).info(f"""       
 
 
-# Start the bot
-if __name__ == "__main__":
-    print("Bot is starting...")
-    app.run()
+  ___ ___  ___  ___ ___ _    _____  _____  ___ _____ ___ 
+ / __/ _ \|   \| __| __| |  |_ _\ \/ / _ )/ _ \_   _/ __|
+| (_| (_) | |) | _|| _|| |__ | | >  <| _ \ (_) || | \__ \
+ \___\___/|___/|___|_| |____|___/_/\_\___/\___/ |_| |___/
+                                                         
+ 
+                                          """)
+        self.username = usr_bot_me.username
+        # web-response
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped.")
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
