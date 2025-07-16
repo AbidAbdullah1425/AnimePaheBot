19e5e6d927057dbaff16992db53b2f94924c3c93
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-07-16 08:37:32 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,10 @@
 #..........Just one requests do not remove my credit..........#
 
 import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
 API_ID = 26254064
 API_HASH = "72541d6610ae7730e6135af9423b319c"
 BOT_TOKEN = ""
@@ -17,3 +21,26 @@ DOWNLOAD_DIR = "./downloads"
 ADMIN = 5296584067
 
 START_PIC = os.environ.get("START_PIC", "https://graph.org/file/dfd1842d8a2dcc536a2b7.jpg https://graph.org/file/b55f0baaa7a6fde7c5682.jpg https://graph.org/file/3298ca8910c82f33418a8.jpg https://graph.org/file/4e4935469a7214c734721.jpg https://graph.org/file/fa5f2b241fe77beff8ba0.jpg https://graph.org/file/bcb9969f78ab4a47a483c.jpg https://graph.org/file/a9af161696d82f17b8888.jpg https://graph.org/file/d23b00650c00ce4d9a467.jpg https://graph.org/file/4dc0b3dfaad61fbcf0a49.jpg https://graph.org/file/7088315e9b0b6a2fa7118.jpg https://graph.org/file/9c9911d06e5f2316febb9.jpg https://graph.org/file/d2ee185180469cfd28071.jpg https://graph.org/file/dea04b2d615406aeb0181.jpg https://graph.org/file/98b63d3bb84984a68cc76.jpg").split()
+
+
+
+
+PORT = os.environ.get("PORT", "8080")
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "1"))
+LOG_FILE_NAME = "filesharingbot.txt"
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
