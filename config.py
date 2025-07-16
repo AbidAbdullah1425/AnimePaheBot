85b45c79df3e5287863e8f0b27359db0a69ad668
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2025-07-16 10:03:40 +0600

    Update config.py

--- config.py
+++ config.py
@@ -7,18 +7,18 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
-API_ID = 26254064
-API_HASH = "72541d6610ae7730e6135af9423b319c"
-BOT_TOKEN = ""
+API_ID = int(os.environ.get("API_ID", "26254064"))
+API_HASH = os.environ.get("API_HASH", "72541d6610ae7730e6135af9423b319c")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", ""
 #Logs
-LOG_CHANNEL = "-1002197279542" 
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002197279542"))
 # MongoDB setup
-MONGO_URL = "mongodb+srv://abidabdullahown7:abidabdullah1425@cluster0.7lgug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-DB_NAME = "AnimeDB"
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://abidabdullahown7:abidabdullah1425@cluster0.7lgug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DB_NAME = os.environ.get("DB_NAME", "AnimeDB"
 
-DOWNLOAD_DIR = "./downloads"
+DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads"
 #ADMIN INFO
-ADMIN = 5296584067
+ADMIN = int(os.environ.get("ADMIN", "5296584067"))
 
 START_PIC = os.environ.get("START_PIC", "https://graph.org/file/dfd1842d8a2dcc536a2b7.jpg https://graph.org/file/b55f0baaa7a6fde7c5682.jpg https://graph.org/file/3298ca8910c82f33418a8.jpg https://graph.org/file/4e4935469a7214c734721.jpg https://graph.org/file/fa5f2b241fe77beff8ba0.jpg https://graph.org/file/bcb9969f78ab4a47a483c.jpg https://graph.org/file/a9af161696d82f17b8888.jpg https://graph.org/file/d23b00650c00ce4d9a467.jpg https://graph.org/file/4dc0b3dfaad61fbcf0a49.jpg https://graph.org/file/7088315e9b0b6a2fa7118.jpg https://graph.org/file/9c9911d06e5f2316febb9.jpg https://graph.org/file/d2ee185180469cfd28071.jpg https://graph.org/file/dea04b2d615406aeb0181.jpg https://graph.org/file/98b63d3bb84984a68cc76.jpg").split()
 
